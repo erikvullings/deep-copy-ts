c83fd8672f3270da2d6f399e36631bffda7f07f6
Erik Vullings <erik.vullings@gmail.com>
2019-12-26 22:57:26 +0100

    Update tsconfig for browser

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * Deep copy function for TypeScript.
  * @param T Generic type of target/copied value.
  * @param target Target value to be copied.
- * @see also ts-deepcopy https://github.com/ykdr2017/ts-deepcopy
+ * @see Orginal source: ts-deepcopy https://github.com/ykdr2017/ts-deepcopy
  * @see Code pen https://codepen.io/erikvullings/pen/ejyBYg
  */
 export const deepCopy = <T>(target: T): T => {
